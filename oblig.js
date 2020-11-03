aa4a45f34c72934e77ee09a7b0948f46ef2b1a00
fluesmekk <theodortberg@gmail.com>
2020-11-03 13:09:21 +0100

    tirsdag

--- oblig.js
+++ oblig.js
@@ -1,118 +1,64 @@
-//model
-var dateNumber = '20.04.2020';
-
-
-
-
-//view
-updateView();
-function updateView() {
-    document.getElementById('app').innerHTML = `
-    
-    <input id="textZone" type="text"> </input>
-    <button onclick="isDateValid(document.getElementById('textZone').value)">skjekk dato</button>
-
-    `;
-}
-
-//controller
 
 function isDateValid(dateString) {
 
-    var dateNumber = dateString;
-
-
-        //tar med seg variablene som deles opp under
-        isLengthCorrect(dateNumber);
-        isDateCorrectlySetUp();
-        isYearCorrect(year);
-        isMonthCorrect(month);
-        isLeapYear(year);
-        isDayCorrect(day, month, leapYear)
+        var dateNumber = dateString;
 
         //her deles opp Datoen du putter inn
         var day = dateNumber.substring(0,2);
         var month = dateNumber.substring(3,5);
-        var year = dateNumber.substring(6, 9);
-        var dots = dateNumber.indexOf('.') === 3 || 6;
-        
+        var year = dateNumber.substring(6, 10);
+    
 
-        if  (lengthCorrect                     === true
-            && dateIsCorrectlySetUp     === true
-            && yearCorrect              === true
-            && monthCorrect             === true
-            && dayCorrect               === true) {
-                return true;
-            } else {
-                return false;
-            }
-
+        //tar med seg variablene ovenifra
+        return isLengthCorrect(dateNumber)  && 
+        isDateCorrectlySetUp(dateNumber)    &&
+        isYearCorrect(year)                 &&
+        isMonthCorrect(month)               &&
+        isDayCorrect(day, month, year);
     
 };
-            var lengthCorrect;
-            var dateIsCorrectlySetUp;
-            var yearCorrect;
-            var dateCorrect;
-            var monthCorrect;
-            var dayCorrect;
-            var leapYear;
-
 
     //skjekker lengde
     function isLengthCorrect(dateLength) {
-        if (dateLength.length === 10) {
-            lengthCorrect = true;
-        }
+        return (dateLength.length == 10);
     };
 
     //skjekker om datorekke er riktig oppsatt med . etter dato og måned.
-    function isDateCorrectlySetUp() {
-        if (dateNumber.indexOf('.') === 3, 6) {
-            dateIsCorrectlySetUp = true;
-        }
-    }
+    function isDateCorrectlySetUp(dateNumber) {
+        return (dateNumber.charAt(2) == '.') && (dateNumber.charAt(5) == '.');
+    };
 
     //skjekker om årstallet er over 0000 og under 9999.
-    function isYearCorrect(yearParameter) {
-        if (yearParameter >= '0000' 
-        && yearParameter <= '9999') {
-            yearCorrect = true;
-        }
-    }
+    function isYearCorrect(year) {
+        return ((parseInt(year) >= 0000) && (parseInt(year) <= 9999) && year.length == 4);
+    };
 
 
     //skjekker om måneden er innefor 01-12
     function isMonthCorrect(monthParameter) {
-        if ((monthParameter >= '01') 
-        && (monthParameter <= '12')) {
-            monthCorrect = true;
-        }
-    }
+        return ((parseInt(monthParameter) >= 01 && parseInt(monthParameter) <= 12) && monthParameter.length == 2);
+    };
 
     //skjekker om det er ett skuddår
+
+    
     function isLeapYear (year) {
-        if ((year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0)) {
-            leapYear = true;
-        }
-    }
+        return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+        
+    };  
     
     //skjekker at datoer passer til måneden.
-    function isDayCorrect(day, month, leapYear) {
-        if (month === '02' && leapYear === false && day <= '29') { 
-            dayCorrect = true;
-        } else if (month !== '02' && day <= '30') {
-            dayCorrect = true;
-        } else if    ((month === '01' || month === '03' 
-                    || month === '05' || month === '07' 
-                    || month === '08' || month === '10' 
-                    || month === '12') && day === '31') {
-            dayCorrect = true;
+    function isDayCorrect(day, month, year) {
+         if  ((month === '04' || month === '06' || month === '09' || month === '11') && (day <= '30' && day >= '01')) {
+            return true; 
+        } else if (month === '02' && isLeapYear(year) && day <= '29') {
+            return true;
+        } else if (month === '02' && !isLeapYear(year) && day >= '29') {
+            return false;
+        } else if ((month === '01' || month === '03' || month === '05' || month === '07' || month === '08' || month === '10' || month === '12') && day <= '31') {
+            return true;
         } else {
-            dayCorrect = false;
+            return false;      
         }
     }
-
-
-
-
-
+      
---
+++ prov.js
@@ -0,0 +1,8 @@
+function check() {
+    //validation code
+    return true;
+  }
+  
+  if(check()) {
+    //Another function code
+   }
